4d8a6ede1c31316afc567363cd7575c5ec60c803
rfk123 <rfkamali@gmail.com>
2023-05-23 14:21:09 -0700

    Minor Footer.js adjustment

--- components/Footer.js
+++ components/Footer.js
@@ -3,8 +3,7 @@ const Footer = () => {
   return (
     <box>
       <h1 style={{textAlign: "center",
-                  marginTop: "-50px",
-                  color: "blue"}}>
+                  marginTop: "-50px"}>
         Ducks Nest: A Hotel Management System
       </h1>
     </box>
