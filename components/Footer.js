c02d7530539a08bb2e483dfbc4872f57f450b05e
rfk123 <rfkamali@gmail.com>
2023-05-23 14:47:47 -0700

    Fixing a bug

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <box>
       <h1 style={{textAlign: "center",
-                  marginTop: "-50px"}>
+                  marginTop: "-50px"}}>
         Ducks Nest: A Hotel Management System
       </h1>
     </box>
