9bd6f1bcf1c73db0da1f8b445fb6148c3c476afb
Luke99326 <lukegscribner@gmail.com>
2023-05-22 17:24:08 -0700

    Added firebase package, init file and dbtest page

---
+++ pages/dbtest.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { db } from '@/firebase/initFirebase'
+import { doc, getDoc, updateDoc } from "firebase/firestore"
+
+const ReadDataFromCloudFirestore = async () => {
+    try {
+        const userDoc = doc(db, "testCollection", "testDoc")
+        await getDoc(userDoc).then((doc) => {
+            if (doc.exists()) {
+                console.log(doc.data())
+            } else {
+                console.log("No such document!");
+            }
+        })
+        alert('Data successfully fetched, check console')
+    } catch (error) {
+        console.log(error)
+        alert(error)
+    }
+}
+
+// const writeToFirestore = async () => {
+//     try {
+//         await updateDoc(doc(db, "testCollection", "testDoc"), {
+//             name: "John Doe"
+//         })
+//     } catch (error) {
+//         console.log(error);
+//         alert(error)
+//     }
+// }
+
+
+
+const dbtest = () => {
+    return (
+        <div>
+            <h1>dbtest</h1>
+            <button onClick={ReadDataFromCloudFirestore}>Read Data From Cloud Firestore</button>
+        </div>
+    );
+}
+
+export default dbtest
