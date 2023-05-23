f57b19f4389224384b86200cd35239e27c7167af
rfk123 <rfkamali@gmail.com>
2023-05-23 10:46:57 -0700

    Updated Footer.js
    Simple footer file

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+const Footer = () => {
+  return {
+    <box>
+      <h1 style={{textAlign: "center",
+                  marginTop: "-50px",
+                  color: "blue"}}>
+        Ducks Nest: A Hotel Management System
+      </h1>
+    </box>
+  };
+};
+export default Footer;
