0579327425313fe14b6c2214490425e4dd0b1520
JulianAlbert12 <122594617+JulianAlbert12@users.noreply.github.com>
2023-05-23 14:10:52 -0700

    Added Navbar + CSS

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,25 @@
-const Navbar = () => {
-    return (
-        <div>
-            <h1> Duck's Nest </h1>
-        </div>
-    )
-}
+import Link from 'next/link';
+import styles from '../styles/NavBar.module.css';
 
-export default Navbar
+const NavBar = () => {
+  return (
+    <nav className={styles.navBar}>
+      <div className={styles.logoContainer}>
+        <img src="/img/TempLogo.png" alt="Logo" className={styles.logo} />
+      </div>
+      <ul className={styles.navList}>
+        <li className={styles.navItem}>
+          <Link href="/">Home</Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link href="/about">About Us</Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link href="/reservation">Reservation</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
