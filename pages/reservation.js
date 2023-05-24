295100c80f977f6b10c97b530c2243f570ce1315
Luke99326 <lukegscribner@gmail.com>
2023-05-24 15:20:55 -0700

    placeholder available rooms on reservation page

--- pages/reservation.js
+++ pages/reservation.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import ResRoom from '@/components/ResRoom';
 
-const AboutPage = () => {
+
+const Reservation = () => {
     const router = useRouter();
     const {checkInDate, checkOutDate} = router.query;
-    console.log(checkOutDate);
+
+    const getAvailRooms = (roomSize) => {
+        // get rooms from database
+        const placeholder = 5
+
+        let avail = []
+        for (let i = 0; i < placeholder; i++) {
+            avail.push(<ResRoom size={roomSize}/>)
+        }
+        return <div>{avail}</div>
+    }
+
+    const genRooms = () => {
+        return [
+            getAvailRooms("Large"),
+            getAvailRooms("Med"),
+            getAvailRooms("Small")
+        ]
+    }
+
+
     return (
         <div>
-        <h1>Bookings</h1>
-        <p>checkIn: {checkInDate}</p>
-        <p>checkOut: {checkOutDate}</p>
-        {/* Add your about content here */}
+            <h1>Bookings</h1>
+            <p>checkIn: {checkInDate}</p>
+            <p>checkOut: {checkOutDate}</p>
+
+            <div style={{display: "flex", gap: "10px"}}>
+                {genRooms()}  
+            </div>
+            
         </div>
     );
 };
 
-export default AboutPage;
+export default Reservation;
