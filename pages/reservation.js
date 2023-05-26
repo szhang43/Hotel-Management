ef95a7f6838583175ea912a2a636d1a360fabb58
McCynthia <cynthiamc87@gmail.com>
2023-05-25 22:41:06 -0700

    Styling Reservation page

--- pages/reservation.js
+++ pages/reservation.js
@@ -29,12 +29,22 @@ const Reservation = () => {
 
 
     return (
-        <div className={style.container}>
-            <h1>Bookings</h1>
-            <p>checkIn: {checkInDate}</p>
-            <p>checkOut: {checkOutDate}</p>
+        <div className={styles.container}>
+            <h1>Reservations</h1>
 
-            <div style={{display: "flex", gap: "10px"}}>
+            <div className={styles.text}>
+            <p> We're delighted to help you find the perfect room and making your stay at our hotel a delightful experience. 
+                Whether you're seeking a cozy nest for a solo adventure<br />
+                or a spacious haven for a family getaway, we have just the right room waiting for you.<br />
+                Take your time, explore our options, and imagine the tranquil ambiance and stunning views that await.
+                Go ahead and find the room that speaks to your heart,<br />
+                and let us create wonderful memories during your stay at Duck's Nest.</p>
+            </div>
+
+            <p className={styles.dates}>You selected the following dates for your stay:<br />
+            Arrival Date: {checkInDate} <br />  Departure Date: {checkOutDate}</p>
+
+            <div className={styles.roomGrid} style={{display: "flex", gap: "30px"}}>
                 {genRooms()}  
             </div>
             
