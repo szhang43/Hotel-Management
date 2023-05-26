8979f6cf6100424097c23f861f7dfcb72c4550f2
McCynthia <cynthiamc87@gmail.com>
2023-05-25 21:05:57 -0700

    About image test

--- pages/about.js
+++ pages/about.js
@@ -8,8 +8,8 @@ const AboutPage = () => {
 
       <Image 
         className={styles.aboutImage}
-          src="/img/about-us-img.jpg"
-          alt="Beach front"
+          src="/img/abt.png"
+          alt="Pool"
           fill
       />
 
@@ -18,22 +18,24 @@ const AboutPage = () => {
       </div>
 
       <div className={styles.aboutus}>
-        <p> Welcome to Duck's Nest, the ultimate destination for adventurous souls seeking a blend of exotic experiences and luxurious accommodations.</p>
+        <p>Welcome to Duck's Nest, the ultimate destination for adventurous souls 
+            <br />seeking a blend of exotic experiences and luxurious accommodations.<br />
 
-        <p> At Duck's Nest, we understand the desires of young travelers who crave unique journeys, exploring far-flung corners of the world while indulging
-          in the comforts of a splendid hotel. </p>
+            At Duck's Nest, we understand the desires of young travelers who crave unique journeys,<br />
+            exploring far-flung corners of the world while indulging in the comforts of a splendid hotel.<br />
+            <br />
+            Our collection of properties offers a harmonious fusion of captivating environments,<br />
+            from lush rainforests to serene beaches and awe-inspiring mountains.<br />
+            Immerse yourself in the wonders of nature and cultural richness.<br />
+            <br />
+            Browse through our selection of rooms and book your ideal retreat with just a few clicks.<br />
 
-        <p> Our carefully curated collection of properties offers a harmonious fusion of captivating environments, from lush rainforests to serene beaches
-          and awe-inspiring mountains. Immerse yourself in the wonders of nature and cultural richness. </p>
+            Whether you seek a cozy cabin in the wilderness or a luxurious suite overlooking breathtaking landscapes,<br />
+            our diverse range of accommodations caters to every taste and preference.<br />
 
-        <p> Browse through our selection of exquisite rooms and book your ideal retreat with just a few clicks. </p>
-
-        <p> Whether you seek a cozy cabin nestled in the wilderness or a luxurious suite overlooking breathtaking landscapes, our diverse range of accommodations
-          caters to every taste and preference.</p>
-
-        <p> Let Duck's Nest be your trusted companion as you create lifelong memories in the world's most enchanting destinations.</p>
-
-        <p> Book your room today and embark on a remarkable experience that will leave you craving for more.</p>
+            Let Duck's Nest be your companion as you create lifelong memories in the world's most enchanting destinations.<br />
+            <br />
+            Book your room today and embark on a remarkable experience that will leave you craving for more.</p>
       </div>
     </div>
   );
