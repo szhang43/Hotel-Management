de2ff430c98d1b3a06289b194ca2ef773aa0c6be
Luke99326 <lukegscribner@gmail.com>
2023-05-26 13:21:27 -0700

    res page gets available rooms from DB

--- pages/reservation.js
+++ pages/reservation.js
@@ -2,29 +2,33 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import ResRoom from '@/components/ResRoom';
 import styles from '@/styles/reservation.module.css';
+import { useState, useEffect } from 'react';
+import { getAvailRooms } from '@/firebase/firebaseUtils';
 
 
 const Reservation = () => {
+    useEffect(() => {
+        updateAvailRooms()
+    }, [])
+
     const router = useRouter();
     const {checkInDate, checkOutDate} = router.query;
+    const [availRooms, setAvailRooms] = useState({large: 0, medium: 0, small: 0})
 
-    const getAvailRooms = (roomSize) => {
-        // get rooms from database
-        const placeholder = 5
-
-        let avail = []
-        for (let i = 0; i < placeholder; i++) {
-            avail.push(<ResRoom size={roomSize}/>)
-        }
-        return <div>{avail}</div>
+    const updateAvailRooms = () => {
+        getAvailRooms()
+        .then((data) => {
+            console.log(data)
+            setAvailRooms(data)
+        })
     }
 
-    const genRooms = () => {
-        return [
-            getAvailRooms("Large"),
-            getAvailRooms("Med"),
-            getAvailRooms("Small")
-        ]
+    const genRooms = (roomSize, numRooms) => {
+        let rooms = []
+        for(let i = 0; i < numRooms; i++){
+            rooms.push(<ResRoom size={roomSize}/>)
+        }
+        return <div className={`${roomSize}Rooms`}>{rooms}</div>
     }
 
 
@@ -33,19 +37,29 @@ const Reservation = () => {
             <h1>Reservations</h1>
 
             <div className={styles.text}>
-            <p> We're delighted to help you find the perfect room and making your stay at our hotel a delightful experience. 
-                Whether you're seeking a cozy nest for a solo adventure<br />
+            <p> We&apos;re delighted to help you find the perfect room and making your stay at our hotel a delightful experience. 
+                Whether you&apos;re seeking a cozy nest for a solo adventure<br />
                 or a spacious haven for a family getaway, we have just the right room waiting for you.<br />
                 Take your time, explore our options, and imagine the tranquil ambiance and stunning views that await.
                 Go ahead and find the room that speaks to your heart,<br />
-                and let us create wonderful memories during your stay at Duck's Nest.</p>
+                and let us create wonderful memories during your stay at Duck&apos;s Nest.</p>
             </div>
 
             <p className={styles.dates}>You selected the following dates for your stay:<br />
             Arrival Date: {checkInDate} <br />  Departure Date: {checkOutDate}</p>
 
+            {/* These can be deleted unless we want to keep them */}
+            <p>Available Large Rooms: {availRooms.large}</p>
+            <p>Available Medium Rooms: {availRooms.medium}</p>
+            <p>Available Small Rooms: {availRooms.small}</p>
+            
+
             <div className={styles.roomGrid} style={{display: "flex", gap: "30px"}}>
-                {genRooms()}  
+                {[
+                    genRooms("large", availRooms.large),
+                    genRooms("medium", availRooms.medium),
+                    genRooms("small", availRooms.small)
+                ]}  
             </div>
             
         </div>
