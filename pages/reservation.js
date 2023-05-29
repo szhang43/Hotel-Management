57acbcf0077e8a2d0a87701b50916a2675b998e2
JulianAlbert12 <122594617+JulianAlbert12@users.noreply.github.com>
2023-05-29 13:55:12 -0700

    update the format of the bookings
    I think having it in a row formation makes more sense for the long descriptions.

--- pages/reservation.js
+++ pages/reservation.js
@@ -33,74 +33,109 @@ const Reservation = () => {
 
 
     return (
-        <div className={styles.container}>
-            <h1>Reservations</h1>
-
-            <div className={styles.text}>
-            <p> We&apos;re delighted to help you find the perfect room and making your stay at our hotel a delightful experience.
-                Whether you&apos;re seeking a cozy nest for a solo adventure<br />
-                or a spacious haven for a family getaway, we have just the right room waiting for you.<br />
-                Take your time, explore our options, and imagine the tranquil ambiance and stunning views that await.
-                Go ahead and find the room that speaks to your heart,<br />
-                and let us create wonderful memories during your stay at Duck&apos;s Nest.</p>
+        <div>
+          <div className={styles.background}>
+            <div className={styles.reservationTitle}>
+              <h1>Reservations</h1> {/* Move the "Reservations" heading into the background */}
             </div>
-
-            <p className={styles.dates}>You selected the following dates for your stay:<br />
-            Arrival Date: {checkInDate} <br />  Departure Date: {checkOutDate}</p>
-
+          </div>
+      
+          <div className={styles.container}>
+            <div className={styles.text}>
+              <p>
+                We&apos;re delighted to help you find the perfect room and making your
+                stay at our hotel a delightful experience. Whether you&apos;re seeking a
+                cozy nest for a solo adventure
+                <br />
+                or a spacious haven for a family getaway, we have just the right room
+                waiting for you.
+                <br />
+                Take your time, explore our options, and imagine the tranquil ambiance
+                and stunning views that await. Go ahead and find the room that speaks to
+                your heart,
+                <br />
+                and let us create wonderful memories during your stay at Duck&apos;s Nest.
+              </p>
+            </div>
+    
+            <p className={styles.dates}>
+              You selected the following dates for your stay:
+              <br />
+              Arrival Date: {checkInDate} <br /> Departure Date: {checkOutDate}
+            </p>
+    
             {/* These can be deleted unless we want to keep them */}
             {/* <p>Available Large Rooms: {availRooms.large}</p>
             <p>Available Medium Rooms: {availRooms.medium}</p>
             <p>Available Small Rooms: {availRooms.small}</p> */}
-
+    
             <div className={styles.roomContainer}>
-                <div className={styles.size}>
-                    <h1>The Cozy | Singles</h1>
-                    <img src="/img/cozy.jpg" alt="Logo" className={styles.img} />
-                    <p>Welcome to our cozy room, designed to provide a comfortable and enjoyable
-                        stay for one or two guests. Relax in a cozy queen-sized bed and appreciate the
-                        tasteful furnishings that create a soothing atmosphere. The en-suite bathroom
-                        offers convenience with modern amenities, including a rain shower and a deep
-                        soaking tub. Enjoy the pleasant ambiance of this well-appointed room, where 
-                        every detail is thoughtfully arranged to ensure a memorable experience.
-                    </p>
-                    <p>Rooms Available: {availRooms.small}</p>
-                    <p>Price: $</p>
-                    <ResRoom size={"small"} checkInDate={checkInDate} checkOutDate={checkOutDate} />
-                </div>
-
-                <div className={styles.size}>
-                    <h1>Room Deluxe | Family </h1>
-                    <img src="/img/deluxe.jpg" alt="Logo" className={styles.img} />
-                    <p>Discover the ultimate in comfort and sophistication within our deluxe room,
-                        thoughtfully designed to accommodate up to four guests. Step into a spacious
-                        sanctuary adorned with stylish furnishings and contemporary touches, creating an
-                        atmosphere of relaxation and indulgence. Sink into the plush beds and relish the
-                        ample space available for your entire group to unwind and rejuvenate. With its inviting
-                        ambiance and attention to detail, this exquisite retreat promises a memorable stay where
-                        comfort and elegance intertwine harmoniously.
-                    </p>
-                    <p>Rooms Available: {availRooms.medium}</p>
-                    <p>Price : $$</p>
-                    <ResRoom size={"medium"} checkInDate={checkInDate} checkOutDate={checkOutDate} />
-                </div>
-
-                <div className={styles.size}>
-                    <h1>Luxury | Group</h1>
-                    <img src="/img/luxury.jpg" alt="Logo" className={styles.img} />
-                    <p>Experience pure luxury in our exquisite room, where elegance meets comfort. Enjoy a king-sized
-                        bed adorned with plush linens for a restful sleep. The spacious en-suite bathroom features marble
-                        accents and indulgent amenities. Immerse yourself in the opulent ambiance of our luxury room and
-                        indulge in an unforgettable stay.
-                    </p>
-                    <p>Rooms Available: {availRooms.large}</p>
-                    <p>Price: $$$ </p>
-                    <ResRoom size={"large"} checkInDate={checkInDate} checkOutDate={checkOutDate} />
-                </div>
+              <div className={styles.size}>
+                <h1>The Cozy | Singles</h1>
+                <img src="/img/cozy.jpg" alt="Logo" className={styles.img} />
+                <p>
+                  Welcome to our cozy room, designed to provide a comfortable and
+                  enjoyable stay for one or two guests. Relax in a cozy queen-sized
+                  bed and appreciate the tasteful furnishings that create a soothing
+                  atmosphere. The en-suite bathroom offers convenience with modern
+                  amenities, including a rain shower and a deep soaking tub. Enjoy
+                  the pleasant ambiance of this well-appointed room, where every
+                  detail is thoughtfully arranged to ensure a memorable experience.
+                </p>
+                <p>Rooms Available: {availRooms.small}</p>
+                <p>Price: $</p>
+                <ResRoom
+                  size={"small"}
+                  checkInDate={checkInDate}
+                  checkOutDate={checkOutDate}
+                />
+              </div>
+    
+              <div className={styles.size}>
+                <h1>Room Deluxe | Family </h1>
+                <img src="/img/deluxe.jpg" alt="Logo" className={styles.img} />
+                <p>
+                  Discover the ultimate in comfort and sophistication within our
+                  deluxe room, thoughtfully designed to accommodate up to four
+                  guests. Step into a spacious sanctuary adorned with stylish
+                  furnishings and contemporary touches, creating an atmosphere of
+                  relaxation and indulgence. Sink into the plush beds and relish the
+                  ample space available for your entire group to unwind and
+                  rejuvenate. With its inviting ambiance and attention to detail,
+                  this exquisite retreat promises a memorable stay where comfort and
+                  elegance intertwine harmoniously.
+                </p>
+                <p>Rooms Available: {availRooms.medium}</p>
+                <p>Price : $$</p>
+                <ResRoom
+                  size={"medium"}
+                  checkInDate={checkInDate}
+                  checkOutDate={checkOutDate}
+                />
+              </div>
+    
+              <div className={styles.size}>
+                <h1>Luxury | Group</h1>
+                <img src="/img/luxury.jpg" alt="Logo" className={styles.img} />
+                <p>
+                  Experience pure luxury in our exquisite room, where elegance meets
+                  comfort. Enjoy a king-sized bed adorned with plush linens for a
+                  restful sleep. The spacious en-suite bathroom features marble
+                  accents and indulgent amenities. Immerse yourself in the opulent
+                  ambiance of our luxury room and indulge in an unforgettable stay.
+                </p>
+                <p>Rooms Available: {availRooms.large}</p>
+                <p>Price: $$$ </p>
+                <ResRoom
+                  size={"large"}
+                  checkInDate={checkInDate}
+                  checkOutDate={checkOutDate}
+                />
+              </div>
             </div>
-
+          </div>
         </div>
-    );
-};
-
-export default Reservation;
+      );
+    };
+    
+    export default Reservation;
