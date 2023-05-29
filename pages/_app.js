7f2747dc6ee39bcb9673da6873f59c923f3adc2d
rfk123 <rfkamali@gmail.com>
2023-05-29 00:17:36 -0700

    Simple implementation of Footer
    Simple Footer. (Needs more work)

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavBar from '../components/Navbar';
+import Footer from '../components/Footer';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
@@ -7,6 +8,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <NavBar />
       <Component {...pageProps} />
+      <Footer />
     </>
   );
 }
