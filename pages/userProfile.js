cf7983fe6d5f6053d2bde0ea7e0fe8c394a23e27
McCynthia <cynthiamc87@gmail.com>
2023-06-01 21:10:33 -0700

    Style profile page

--- pages/userProfile.js
+++ pages/userProfile.js
@@ -15,28 +15,29 @@ function UserProfile(){
     }, []);
 
     return (
-        <div className = {styles.userProfile}>
-            {user ? (
-                <div>
-                    <h3>Welcome, {user.displayName}!</h3>
-                    <h4>Profile</h4>
-                    <span>First Name: {user.displayName.split(" ")[0]}</span>
-                    <span>Last Name: {user.displayName.split(" ")[1]}</span>
-                    <p>Email: {user.email}</p>
-                    <p>Phone Number: {user.phoneNumber}</p>
+        <div className={styles.background}>
+            <div className = {styles.userProfile}>
+                {user ? (
+                    <div>
+                        <h2 className={styles.welcome}>Welcome, {user.displayName}!</h2>
+                        <h4 className={styles.prof}>Profile</h4>
+                        <span>First Name: {user.displayName.split(" ")[0]+(" ")}</span>
+                        <span>Last Name: {user.displayName.split(" ")[1]}</span>
+                        <p>Email: {user.email}</p>
+                        <p>Phone Number: {user.phoneNumber}</p>
 
-                    <h4>Reservations</h4>
-                    <Reservation/>
-                    <button className={styles.signout} onClick={(event) => signOut(auth)}>Sign out</button>
-                </div>
-                
-            ) : (
-                <p>Please <Link href="login"> Login </Link> 
-                or <Link href="login"> create </Link>  an account</p>
-            )}
-            <Link href="/">Back</Link>
+                        <h4 className={styles.res}>Reservations</h4>
+                        <Reservation/>
+                        <button className={styles.signout} onClick={(event) => signOut(auth)}>Sign out</button>
+                    </div>
+                    
+                ) : (
+                    <p>Please <Link href="login"> Login </Link> 
+                    or <Link href="login"> create </Link>  an account</p>
+                )}
+                <Link className={styles.goback} href="/">Back</Link>
+            </div>
         </div>
-   
     );
 }
 
