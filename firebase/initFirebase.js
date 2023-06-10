bf5385401075aea9b121a334a760289dfd77146f
luke99326 <lukegscribner@gmail.com>
2023-06-09 17:17:40 -0700

    comments in initfirebase

--- firebase/initFirebase.js
+++ firebase/initFirebase.js
@@ -1,3 +1,10 @@
+/*
+    filename: initFirebase.js
+    description: initializes the firestore database and firebase user authentication service 
+        (provided by firebase)
+*/
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
